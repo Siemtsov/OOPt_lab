39e109528fb40ef270e121b307cb185cde695cbc
Ivan <vaniysemtsov@gmail.com>
2024-02-21 18:27:22 +0200

    Testing Classes in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "Person.h"
+#include "Place.h"
+#include "Date.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Person person1("Ivan",17,"Male");
+    person1.getPerson();
+    person1.setName("Anton");
+    person1.getPerson();
+    std::cout<<std::endl;
+    Place place1("City Center", "Sunny", 15);
+    place1.getPlace();
+    place1.setPlace("Town outskirts");
+    place1.getPlace();
+    std::cout<<std::endl;
+    Date date1("Tuesday",03,2025);
+    date1.getDate();
+    date1.setDay("Wednesday");
+    date1.getDate();
+
     return 0;
 }

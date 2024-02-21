b2f01d7a43c48d1d69b5036f8ee9d953de34c2ad
Ivan <vaniysemtsov@gmail.com>
2024-02-21 18:26:23 +0200

    MarkUp Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,5 +1,19 @@
-//
-// Created by Admin on 21.02.2024.
-//
 
+#include <iostream>
 #include "Date.h"
+
+void Date:: getDate(){
+    std::cout<<"Day of week: "<<day<<std::endl
+             <<"Month: "<<month<<std::endl
+             <<"Year: "<<year<<std::endl;
+}
+
+void Date::setDay(std::string &&newDay) {
+    day=newDay;
+}
+
+Date::Date(): Date("Monday",01,2024) {}
+Date::Date(std::string newDay, int newMonth, int newYear)
+        :day{newDay},month{newMonth},year{newYear}
+{}
+Date::~Date(){}

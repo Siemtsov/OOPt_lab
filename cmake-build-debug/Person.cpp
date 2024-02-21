17b2f43ebd9299dc94b2792a8af30e27915a9abb
Ivan <vaniysemtsov@gmail.com>
2024-02-21 18:18:03 +0200

    MarkUp Person CPP

--- cmake-build-debug/Person.cpp
+++ cmake-build-debug/Person.cpp
@@ -1,5 +1,32 @@
-//
-// Created by Admin on 20.02.2024.
-//
 
 #include "Person.h"
+#include <iostream>
+void Person::getPerson() {
+    std::cout << "Name: " << name << std::endl
+              << "Age: " << age << std::endl
+              << "Sex: " << sex << std::endl;
+}
+void Person::setName(std::string &&newName) {
+    name = newName;
+}
+
+Person::Person()
+//        : name{"None"}, age{0}, sex{"Unknown"} lists of initialization
+        : Person("Unknown", 0, "Unknown") {}
+
+Person::Person(std::string newName)
+//        : name{newName}, age{0}, sex{"Unknown"}
+        : Person(newName, 0, "Unknown") {}
+
+Person::Person(std::string newName, int newAge)
+//        : name{newName}, age{newAge}, sex{"Unknown"}
+        : Person(newName, newAge, "Unknown") {}
+
+Person::Person(std::string newName, int newAge, std::string newSex)// Delegating constructor
+        : name{newName}, age{newAge}, sex{newSex} {
+//    std::cout << "Name: " << newName << std::endl
+//              << "Age: " << newAge << std::endl
+//              << "Sex: " << newSex << std::endl;
+}
+
+Person::~Person() {}

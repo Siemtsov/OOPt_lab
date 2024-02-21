7da86c031b584e74dc94909c66119692e811a91c
Ivan <vaniysemtsov@gmail.com>
2024-02-21 18:24:49 +0200

    MarkUp Place.cpp

--- Place.cpp
+++ Place.cpp
@@ -1,5 +1,25 @@
-//
-// Created by Admin on 21.02.2024.
-//
 
+#include <iostream>
 #include "Place.h"
+void Place::getPlace() {
+    std::cout << "Place: " << name << std::endl
+              << "Weather type: " << weather << std::endl
+              << "Temperature: " << temperature << std::endl;
+}
+void Place::setPlace(std::string &&newName) {
+    name=newName;
+}
+
+Place::Place()
+//:name{"Unknown Place"},weather{"Unknown Weather"},temperature{0}
+        : Place("Unknown Place","Unknown weather",0)
+{}
+Place::Place(std::string newPlace, std::string newWeather)
+//:name{newPlace},weather{newWeather},temperature{0}
+        :Place(newPlace,newWeather,0)
+{}
+Place::Place(std::string newPlace,std::string newWeather,int newTemperature)
+        :name{newPlace},weather{newWeather},temperature{newTemperature}
+{
+}
+Place::~Place(){};
